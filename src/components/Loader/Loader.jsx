15563055b9d6f8a6e3cd317520d7e4098b4dd322
AniaPiWo <snowflake@wp.pl>
2023-02-15 12:03:27 +0100

    css fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,19 +1,8 @@
-import { ThreeCircles } from 'react-loader-spinner';
+import { InfinitySpin } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
 export const Loader = () => (
   <div className={css.Loader}>
-    <ThreeCircles
-      height="50"
-      width="50"
-      color="hotpink"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-      ariaLabel="three-circles-rotating"
-      outerCircleColor=""
-      innerCircleColor=""
-      middleCircleColor=""
-    />
+    <InfinitySpin width="200" color="hotpink" />
   </div>
 );
